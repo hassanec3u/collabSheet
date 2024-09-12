01c1679835905c5febc756f6f74e07679357f985
abou <hassanec3u@etu.univ-lorraine.fr>
2024-09-12 17:37:18 +0200

    base de donné Mysql configuré et possible et possibilté d'inscrire un user

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
-// server.js
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2');
 
 // Configuration du moteur de rendu
 app.set('view engine', 'ejs');
@@ -15,6 +15,22 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Middleware pour récupérer les données du formulaire
 app.use(express.urlencoded({ extended: true }));
 
+// Configuration de la connexion à la base de données
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'phpmyadmin',
+    password: 'abou',
+    database: 'collabSheet'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données:', err);
+    } else {
+        console.log('Connecté à la base de données MySQL');
+    }
+});
+
 // Page d'accueil
 app.get('/', (req, res) => {
     res.render('index');
@@ -24,14 +40,24 @@ app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+// Route pour gérer l'inscription
+app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    const query = 'INSERT INTO users (nom, motDePasse) VALUES (?, ?)';
+    db.query(query, [username, password], (err, result) => {
+        if (err) {
+            console.error('Erreur lors de l\'insertion des données:', err);
+            res.status(404).send('Erreur lors de l\'inscription');
+        } else {
+            console.log("utilisateur ajouté");
+            res.render('success');
+        }
+    });
+});
+
 // toutes les autres routes qui ne sont pas défini
 app.get('*', (req, res) => {
     res.send('Page introuvable');
-});
-
-app.post('/signup', (req, res) => {
-    const { username, password } = req.body;
-    res.render('success');
 });
 
 // Démarrer le serveur
